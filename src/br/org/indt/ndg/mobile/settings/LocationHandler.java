f99b58bf3894ffdeb19fe47a3ac69d8b46faed11
varuna <varuna@Varuna-Toughra>
2013-05-08 16:13:35 +0400

    Added location logic and code for J2ME / non GPS devices  (like Nokia 112) to get location from Cellular ID triangulation.

--- src/br/org/indt/ndg/mobile/settings/LocationHandler.java
+++ src/br/org/indt/ndg/mobile/settings/LocationHandler.java
@@ -8,6 +8,20 @@ import javax.microedition.location.LocationListener;
 import javax.microedition.location.LocationProvider;
 import javax.microedition.location.LocationException;
 
+//new libraries imported for cellid capture logic. Note: CellID logic in Nokia phones uses Data (small text).
+// A new dummy LocationUtil (which is part of Nokia phones) was created for project to run in Netbeans.
+import com.nokia.mid.location.LocationUtil;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+import javax.microedition.lcdui.Form;
+import javax.microedition.location.Coordinates;
+import javax.microedition.location.QualifiedCoordinates;
+//end of adding new libraries change.
+
 public class LocationHandler implements LocationListener {
 
     private Criteria criteria = null;
@@ -17,6 +31,10 @@ public class LocationHandler implements LocationListener {
     volatile private boolean updateProvider = false;;
     private int currentState = 0;
     private Thread tUpdate = null;
+    
+    //Adding a new thread which will keep running. Purpose to keep obtaining cell tower coordinates.
+    Thread locationWatch;
+    //end of thread initialisation. 
 
     public LocationHandler() {
     }
@@ -25,15 +43,28 @@ public class LocationHandler implements LocationListener {
         lastLocation.set(null);
         locationPresent = false;
         currentState = 0;
-        try {
-            provider = LocationProvider.getInstance(criteria);
-            if (provider != null) {
-                provider.setLocationListener(this, -1, -1, 0);
-                currentState = provider.getState();
-            }
-        } catch (LocationException e) {
-        } catch (SecurityException e) {
+        
+        //New addition: To initiate the locationWatch thread.
+        //Thread calls LocationWatch() and sets priority to Minimum (so that it can run in the background).
+        provider = null; // Reset to null.
+        
+        if(locationWatch == null) { //locationWatch initialised in the main class.
+            locationWatch = new Thread(new LocationWatch());    // New thread that starts new class LocationWatch().
+            locationWatch.setPriority(Thread.MIN_PRIORITY);
+            locationWatch.start();  // Will call class LocationWatch.run().
         }
+        
+        //end of change.
+        
+ //       try {
+ //           provider = LocationProvider.getInstance(criteria);
+ //           if (provider != null) {
+ //               provider.setLocationListener(this, -1, -1, 0);
+ //               currentState = provider.getState();
+ //           }
+ //       } catch (LocationException e) {
+ //       } catch (SecurityException e) {
+ //      }
         return currentState;
     }
 
@@ -77,6 +108,15 @@ public class LocationHandler implements LocationListener {
         }
     }
 
+    // Adding a new method to start a thread for the CellId method of location grab.
+    // This will be initiated when user pressed Update Location button in the Survey.
+    public void updateLocation() {
+        
+        tUpdate.run(); 
+        
+    }
+    // End of change.
+    
     public void updateServiceOff() {
         updateProvider = false;
 //        if (tUpdate != null && tUpdate.isAlive()) {
@@ -111,6 +151,43 @@ public class LocationHandler implements LocationListener {
         public synchronized void set(Location l) { loc = l; }
     }
 
+     /**
+     * For Nokia J2ME feature phones that use network location they do not seem
+     * to fire locationUpdated events.  This class will constantly request the 
+     * location and fire an event when the location coordinates change
+     */
+    class LocationWatch implements Runnable {
+        
+        public boolean running = true;
+        
+        QualifiedCoordinates lastCoord;
+        
+        public void run () {
+            while(running) {
+                try {
+                    int[] methods = {(Location.MTA_ASSISTED | Location.MTE_CELLID | Location.MTE_SHORTRANGE | Location.MTY_NETWORKBASED)};
+                    // Retrieve the location provider
+                    provider = LocationUtil.getLocationProvider(methods, null);
+                    Location loc =provider.getLocation(50000);
+                    QualifiedCoordinates coord = loc.getQualifiedCoordinates();
+                    locationPresent = true;
+                    currentState = LocationProvider.AVAILABLE;
+                    
+                    if(!coord.equals(lastCoord)) {
+                        locationUpdated(provider, loc);
+                    }
+                    
+                    // Change this value for the interval to the desired location update to happen.
+                    // in ms. 60000=60 seconds=1 minute.
+                    Thread.sleep(60000);
+                }catch(Exception e) {}
+            } // In our and this case, this loop runs indefinitly with sleep cycles.
+            
+        }
+    }
+    
+// End of change.
+    
 // in case that of connecting bluetooth gps
         class UpdateProvider implements Runnable {
 
